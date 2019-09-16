ec188eb92a066f275084684391c64f8ff27bc74a
knight42 <zengjian@iftech.io>
2019-09-16 19:19:34 +0800

    feat(git): choose whether to dump error

--- pkg/git/git.go
+++ pkg/git/git.go
@@ -1,35 +1,42 @@
 package git
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"strings"
 )
 
-func getOutput(name string, arg ...string) (string, error) {
-	cmd := exec.Command(name, arg...)
-	cmd.Stderr = os.Stderr
+func getOutput(dumpError bool, args ...string) (string, error) {
+	cmd := exec.Command(args[0], args[1:]...)
 	data, err := cmd.Output()
+	if err != nil && dumpError {
+		if ee, ok := err.(*exec.ExitError); ok {
+			c := strings.Join(args, " ")
+			fmt.Fprintf(os.Stderr, "ERROR: `%s`: %s", c, string(ee.Stderr))
+		}
+	}
 	return strings.TrimSpace(string(data)), err
 }
 
 func HasChanges() bool {
-	err := exec.Command("git", "diff", "--quiet").Run()
-	return err != nil
+	cmd := exec.Command("git", "diff", "--quiet")
+	cmd.Stderr = os.Stderr
+	return cmd.Run() != nil
 }
 
 func GetOriginURL() (string, error) {
-	return getOutput("git", "config", "--get", "remote.origin.url")
+	return getOutput(true, "git", "config", "--get", "remote.origin.url")
 }
 
 func GetCurrentBranch() (string, error) {
-	return getOutput("git", "rev-parse", "--abbrev-ref", "HEAD")
+	return getOutput(true, "git", "rev-parse", "--abbrev-ref", "HEAD")
 }
 
 func GetAbbrevCommitHash() (string, error) {
-	return getOutput("git", "rev-parse", "--short", "HEAD")
+	return getOutput(true, "git", "rev-parse", "--short", "HEAD")
 }
 
 func GetTagOfCommit(commitHash string) (string, error) {
-	return getOutput("git", "describe", "--exact-match", "--tags", commitHash)
+	return getOutput(false, "git", "describe", "--exact-match", "--tags", commitHash)
 }
