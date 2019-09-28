ce74546e919414bc8b52c7b4ccceb2d314f9367e
knight42 <zengjian@iftech.io>
2019-09-28 18:00:37 +0800

    feat: support dockerhub

---
+++ pkg/registry/dockerhub.go
@@ -0,0 +1,58 @@
+package registry
+
+import (
+	"fmt"
+
+	"github.com/docker/docker/api/types"
+)
+
+// DockerHubRegistry can be any registry which is compatible with Docker Hub.
+type DockerHubRegistry struct {
+	Server   string `json:"server" yaml:"server"`
+	Username string `json:"username" yaml:"username"`
+	Password string `json:"password" yaml:"password"`
+}
+
+var _ innerRegistryInterface = (*DockerHubRegistry)(nil)
+
+func (r *DockerHubRegistry) CreateRepoIfNotExists(repo string) error {
+	return nil
+}
+
+func (r *DockerHubRegistry) Prefix() string {
+	if len(r.Server) == 0 {
+		return r.Username
+	}
+	return r.Server
+}
+
+func (r *DockerHubRegistry) GetLatestTag(repo string) (string, error) {
+	return "latest", nil
+}
+
+func (r *DockerHubRegistry) Verify() error {
+	if len(r.Username) == 0 || len(r.Password) == 0 {
+		return fmt.Errorf("empty username or password")
+	}
+	return nil
+}
+
+func (r *DockerHubRegistry) GetAuthConfig() (types.AuthConfig, error) {
+	var addr string
+	if len(r.Server) == 0 {
+		// However (for legacy reasons) the “official” Docker, Inc. hosted registry
+		// must be specified with both a “https://” prefix and a “/v1/” suffix
+		// even though Docker will prefer to use the v2 registry API.
+
+		// See also https://docs.docker.com/engine/api/v1.40/#operation/ImageBuild
+		addr = "https://index.docker.io/v1/"
+	} else {
+		addr = r.Server
+	}
+	auth := types.AuthConfig{
+		ServerAddress: addr,
+		Username:      r.Username,
+		Password:      r.Password,
+	}
+	return auth, nil
+}
